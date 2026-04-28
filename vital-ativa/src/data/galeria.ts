729e1f9d37b90deea4aef3e164b08eaeb6c6d6ff
DevTavynho <octavioalvesfla@gmail.com>
2026-04-27 21:44:25 -0300

    create file galeria.ts

---
+++ vital-ativa/src/data/galeria.ts
@@ -0,0 +1,54 @@
+import type { FotoGaleria } from "@/types";
+
+export const galeria: FotoGaleria[] = [
+  {
+    id: "g1",
+    src: "https://images.unsplash.com/photo-1534438327276-14e5300c3a48?w=1280&q=80",
+    alt: "Sala de musculação com equipamentos modernos",
+  },
+  {
+    id: "g2",
+    src: "https://images.unsplash.com/photo-1571902943202-507ec2618e8f?w=1280&q=80",
+    alt: "Área de cross training com barras e argolas",
+  },
+  {
+    id: "g3",
+    src: "https://images.unsplash.com/photo-1540497077202-7c8a3999166f?w=1280&q=80",
+    alt: "Sala de spinning iluminada com bikes profissionais",
+  },
+  {
+    id: "g4",
+    src: "https://images.unsplash.com/photo-1518611012118-696072aa579a?w=1280&q=80",
+    alt: "Sala de pilates com aparelhos reformer",
+  },
+  {
+    id: "g5",
+    src: "https://images.unsplash.com/photo-1599901860904-17e6ed7083a0?w=1280&q=80",
+    alt: "Estúdio de yoga amplo e silencioso",
+  },
+  {
+    id: "g6",
+    src: "https://images.unsplash.com/photo-1571019613454-1cb2f99b2d8b?w=1280&q=80",
+    alt: "Área funcional com equipamentos variados",
+  },
+  {
+    id: "g7",
+    src: "https://images.unsplash.com/photo-1581009146145-b5ef050c2e1e?w=1280&q=80",
+    alt: "Recepção acolhedora com iluminação natural",
+  },
+  {
+    id: "g8",
+    src: "https://images.unsplash.com/photo-1550345332-09e3ac987658?w=1280&q=80",
+    alt: "Vestiário amplo com armários individuais",
+  },
+  {
+    id: "g9",
+    src: "https://images.unsplash.com/photo-1574680096145-d05b474e2155?w=1280&q=80",
+    alt: "Área de aquecimento com esteiras e elípticos",
+  },
+  {
+    id: "g10",
+    src: "https://images.unsplash.com/photo-1517836357463-d25dfeac3438?w=1280&q=80",
+    alt: "Espaço de alongamento com colchonetes",
+  },
+];
